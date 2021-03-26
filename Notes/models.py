f0c2aaf0b90410975dd2c5e48708948b0d1565a9
dankazim <itskazimz@gmail.com>
2021-03-26 15:07:28 +0700

    added some tests

--- Notes/models.py
+++ Notes/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 from django.utils import timezone
+import datetime
 
 class NoteBook(models.Model):
     title = models.CharField(max_length=20, primary_key=True )
     pub_date = models.DateTimeField('Date Created',auto_now_add=True, blank=True)
     plain_note = models.TextField(blank=True, max_length=1040)
     update_date = models.DateTimeField('Date Modified', auto_now=True)
+
+    def was_created_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    def was_updated_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.update_date <= now
+
     def __str__ (self):
         return "{}".format(self.title)
--- Notes/tests.py
+++ Notes/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from .models import NoteBook
+from django.utils import timezone
+import datetime
+from django.urls import reverse
 
-# Create your tests here.
+class CreateAndUpdateTests(TestCase):
+    def test_created_recently(self):
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_note = NoteBook(pub_date=time)
+        self.assertIs(recent_note.was_created_recently(), True)
+
+    def test_updated_recently(self):
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_note = NoteBook(update_date=time)
+        self.assertIs(recent_note.was_updated_recently(), True)
+    
+
+""" def create_note(title):
+    time = timezone.now()
+    return NoteBook.objects.create(title=title, pub_date=time)
+
+class CreateTests(TestCase):
+    def test_duplicates(self):
+        create_note(title="dan")
+        response = self.client.get(reverse('Notes:index'))
+        self.assertQuerysetEqual(
+            response.context['Notes_list'],
+            ['<Question: Past question.>']
+        ) """
